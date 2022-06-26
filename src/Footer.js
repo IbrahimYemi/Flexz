7bdf383a007433d941574ba178a460d009a565d1
IbrahimYemi <sharafadeen.might@gmail.com>
2022-06-26 13:28:51 +0100

    2 files modified

--- src/Footer.js
+++ src/Footer.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faTwitter,
+  faGithub,
+  faInstagram,
+  falinkedin,
+} from '@fortawesome/free-solid-svg-icons';
 
 export default function Footer() {
+  const Twitter = <FontAwesomeIcon icon={faTwitter} />;
+  const Github = <FontAwesomeIcon icon={faGithub} />;
+  const Instagram = <FontAwesomeIcon icon={fInstagram} />;
+  const Linkedin = <FontAwesomeIcon icon={faLinkedin} />;
   return (
     <div className="footer">
       <div className="social">
-        <i className="fa fa-twitter"></i>
-        <i className="fa fa-github"></i>
-        <i className="fa fa-instagram"></i>
-        <i className="fa fa-linkedin"></i>
+        {Twitter}
+        {Github}
+        {Instagram}
+        {Linkedin}
       </div>
       <div className="hr"></div>
       <div className="copyright">&copy; of Ibrahim Yemi 2022</div>
